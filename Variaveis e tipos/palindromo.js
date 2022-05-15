ec05d2e8a13efed7793176a89a27b1f4e1970769
flanaomi <flavia_ando@hotmail.com>
2022-05-15 18:42:45 +0900

    Aula de variaveis e tipos

---
+++ Variaveis e tipos/palindromo.js
@@ -0,0 +1,34 @@
+
+let myVar = 'a dama admirou o rim da amada'
+
+myVar = myVar .split(" ") .join("");
+
+// function verificarPalindromo (string){
+//     if (!string) return "string inexistente" ;
+
+    
+    
+//    return string  .split("").reverse().join("") === string;
+
+// }
+
+
+
+
+// console.log (verificarPalindromo (myVar))
+
+
+function verificarPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for( var i = 0; i < string.length / 2; i++) {
+        if (string [i] !== string[string.length -1 - i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log (verificarPalindromo2 (myVar))
+
